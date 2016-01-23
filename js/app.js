75f393fc088a950cb285419fdd1aadf25e162c68
Yusuf Emre Durur <ydurur@ebay.com>
2016-01-23 16:43:26 +0200

    example  ng-options added to project

--- js/app.js
+++ js/app.js
@@ -2,38 +2,49 @@
  * Created by ydurur on 23/01/16.
  */
 
-var app = angular.module('expensesApp', []);
+var app = angular.module('exampleApp', []);
 
-app.controller('ExpensesViewController', ['$scope', function($scope){
+app.controller('ExampleViewController', ['$scope', function($scope){
     $scope.name = 'Yusuf Emre Durur';
     $scope.expense = {
         description : 'food',
         amount : 10
     };
 
+    $scope.selectedItem = null;
+
     $scope.increaseAmount = function () {
         $scope.expense.amount++;
-    }
+    };
 
     $scope.items = [
         {
+            id: 1,
             name: 'pizza',
             ingredients: ['cheese', 'tomato', 'oregano', 'salt']
         },
 
         {
+            id: 2,
             name: 'tortilla',
             ingredients: ['butter', 'salt', 'pepper', 'garlic']
         },
 
         {
+            id: 3,
             name: 'cake',
             ingredients: ['cream', 'sugar']
         },
 
         {
+            id: 4,
             name: 'empanada',
             ingredients: ['flour', 'meat', 'onion']
         }
     ];
+
+    $scope.selectFood = function ($foodId) {
+        $scope.selectedItem = $scope.items[$foodId-1].ingredients;
+    };
+
 }]);
