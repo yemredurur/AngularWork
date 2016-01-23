190ec48a6da071a6843914fa5a0b717eb28b5381
Yusuf Emre Durur <ydurur@ebay.com>
2016-01-23 15:11:50 +0200

    adding angular js and html to project

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ydurur on 23/01/16.
+ */
+
+var app = angular.module('expensesApp', []);
+
+app.controller('ExpensesViewController', ['$scope', function($scope){
+    $scope.name = 'Yusuf Emre Durur';
+}]);
