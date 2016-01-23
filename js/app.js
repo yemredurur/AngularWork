961d268a838e6daa2dc86497f5a002be675df301
Yusuf Emre Durur <ydurur@ebay.com>
2016-01-23 15:45:51 +0200

    adding angular js examples

--- js/app.js
+++ js/app.js
@@ -6,4 +6,34 @@ var app = angular.module('expensesApp', []);
 
 app.controller('ExpensesViewController', ['$scope', function($scope){
     $scope.name = 'Yusuf Emre Durur';
+    $scope.expense = {
+        description : 'food',
+        amount : 10
+    };
+
+    $scope.increaseAmount = function () {
+        $scope.expense.amount++;
+    }
+
+    $scope.items = [
+        {
+            name: 'pizza',
+            ingredients: ['cheese', 'tomato', 'oregano', 'salt']
+        },
+
+        {
+            name: 'tortilla',
+            ingredients: ['butter', 'salt', 'pepper', 'garlic']
+        },
+
+        {
+            name: 'cake',
+            ingredients: ['cream', 'sugar']
+        },
+
+        {
+            name: 'empanada',
+            ingredients: ['flour', 'meat', 'onion']
+        }
+    ];
 }]);
