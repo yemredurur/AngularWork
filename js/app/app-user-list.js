4ac71d3b47744c2120880f35d30ee9a3fe02515b
Yusuf Emre Durur <ydurur@ebay.com>
2016-01-25 23:12:24 +0200

    editable show hide

--- js/app/app-user-list.js
+++ js/app/app-user-list.js
@@ -2,79 +2,77 @@
  * Created by ydurur on 23/01/16.
  */
 
-var app = angular.module('userListApp',  ["firebase"]),
-    fireBaseUrl = "https://shining-torch-9442.firebaseio.com/";
-
-
-
-app.controller('userListViewController', ['$scope','userLists',
-    function($scope, userLists){
-        $scope.usersFirebase = new Firebase(fireBaseUrl+'/users');
-
-        console.log(userLists);
-
-        $scope.usersFirebase.on('value', function(data) {
-
-            $scope.safeApply(function() {
-                $scope.usersList = data.val();
-                $scope.usersListTemp = data.val();
-            });
-        });
-
-
-
-        $scope.safeApply = function(fn) {
-            var phase = this.$root.$$phase;
-            if(phase == '$apply' || phase == '$digest') {
-                if(fn && (typeof(fn) === 'function')) {
-                    fn();
-                }
-            } else {
-                this.$apply(fn);
-            }
-        };
-
-
-        $scope.addUser = function () {
-            $scope.usersFirebase.push({
-                id: $scope.userId,
-                name: $scope.userName,
-                phone: $scope.userPhone,
-                email : $scope.userEmail,
-                location: {
-                    city: "San Francisco",
-                    state: "California",
-                    zip: 94103
-                }
-            });
-        };
-
-        $scope.deleteUser = function(key){
-
-            var fredRef = new Firebase(fireBaseUrl+'/users/'+key);
-            var onComplete = function(error) {
-                if (error) {
-                    console.log('Synchronization failed');
-                } else {
-                    console.log('Synchronization succeeded');
-                }
-            };
-
-            fredRef.remove(onComplete);
-        }
-    }
-]);
+var app = angular.module('userListApp', ["firebase"]),
+    fireBaseUrl = "https://shining-torch-9442.firebaseio.com";
 
 
 app.factory("userLists", ["$firebaseArray",
     function($firebaseArray) {
         // create a reference to the database location where we will store our data
-        console.log(fireBaseUrl);
-        var randomRoomId = Math.round(Math.random() * 100000000);
-        var ref = new Firebase(fireBaseUrl+'users' + randomRoomId);
+        var ref = new Firebase(fireBaseUrl+'/users');
         // this uses AngularFire to create the synchronized array
         return $firebaseArray(ref);
     }
 ]);
 
+app.controller('userListViewController', ['$scope', '$firebaseObject', 'userLists',
+    function($scope, $firebaseObject, userLists){
+        $scope.usersList = userLists;
+        $scope.showAdded = false;
+
+        $scope.addUser = function () {
+            $scope.usersList.$add({
+                id: $scope.userId,
+                name: $scope.userName,
+                phone: $scope.userPhone,
+                email : $scope.userEmail,
+                address: {
+                    city: "San Francisco",
+                    street: "California",
+                    suite: "Apt. 692",
+                    zipcode: 94103
+                }
+            });
+            $scope.showAdded = true;
+
+            setTimeout(function(){
+                $scope.showAdded = false;
+                console.log('aaaaaaa');
+            }, 2000);
+        };
+
+        $scope.deleteUser = function(key){
+            var userRef = new Firebase(fireBaseUrl+'/users/'+key);
+            var obj = $firebaseObject(userRef);
+            obj.$remove().then(function(ref) {
+                console.log('data has been deleted locally and in the database');
+            }, function(error) {
+                console.log("Error:", error);
+            });
+        };
+
+        $scope.editUser = function(user){
+            var edit = 'showEdit'+user.id;
+            $scope[ edit ] = true
+        };
+
+        $scope.saveUser = function(user){
+            var edit = 'showEdit'+user.id;
+            $scope[ edit ] = false;
+
+            var userRef = new Firebase(fireBaseUrl+'/users/'+user.$id);
+            var obj = $firebaseObject(userRef);
+                obj.id = $scope[ edit ].editUserId,
+                obj.name = $scope[ edit ].editUserName,
+                obj.phone = $scope[ edit ].editUserPhone,
+                obj.email = $scope[ edit ].editUserEmail
+
+            obj.$save().then(function(ref) {
+                console.log('data has been deleted locally and in the database');
+            }, function(error) {
+                console.log("Error:", error);
+            });
+        };
+    }
+]);
 
